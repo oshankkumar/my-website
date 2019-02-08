38225aef45ecec9d70d5d7a04ed460911d2806c1
Oshank Kumar <okumar@redhat.com>
2019-02-08 21:12:13 +0530

    added request logging

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ FROM alpine:latest
 RUN apk --no-cache add ca-certificates
 WORKDIR /root/
 COPY --from=build /go/src/github.com/oshankkumar/app/ .
+ENV PORT=8080
 CMD ["./app"]
--- main.go
+++ main.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"net"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gorilla/handlers"
 	"github.com/sirupsen/logrus"
 )
+
+var (
+	port = flag.String("port", os.Getenv("PORT"), "application server port")
+)
+
+func main() {
+	flag.Parse()
+
+	http.HandleFunc("/health", health)
+	http.Handle("/", http.FileServer(http.Dir("web")))
+	mux := AddMiddleware(http.DefaultServeMux, Logging)
+
+	logrus.WithField("addr", ":"+*port).Info("starting server")
+	logrus.Fatal(http.ListenAndServe(":"+*port, mux))
+}
+
+func getLocalIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		logrus.WithError(err).Error("error in dialing google public dns")
+		return ""
+	}
+	defer conn.Close()
+	addr := conn.LocalAddr().String()
+	return strings.Split(addr, ":")[0]
+}
 
 func health(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -24,21 +53,9 @@ func Logging(next http.Handler) http.Handler {
 	return handlers.LoggingHandler(logrus.NewEntry(logrus.StandardLogger()).Writer(), next)
 }
 
-func main() {
-	ipAddr := getLocalIP()
-	logrus.WithField("ip", ipAddr).Info("starting server")
-	http.HandleFunc("/health", health)
-	http.Handle("/", http.FileServer(http.Dir("web")))
-
-	logrus.Fatal(http.ListenAndServe(":8080", Logging(http.DefaultServeMux)))
-}
-
-func getLocalIP() string {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		logrus.WithError(err).Error("error in dialing google public dns")
-		return ""
+func AddMiddleware(base http.Handler, middlewares ...func(http.Handler) http.Handler) http.Handler {
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		base = middlewares[i](base)
 	}
-	defer conn.Close()
-	return conn.LocalAddr().String()
+	return base
 }
